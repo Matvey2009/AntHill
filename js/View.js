18e08860eff611316cf5207b603e435dabcb0c2b
Matvey2009 <matvey2009ua@gmail.com>
2022-03-18 17:50:09 +0300

    Update View.js

--- js/View.js
+++ js/View.js
@@ -3,12 +3,14 @@
 class View {
     constructor() {
         this.canvas = document.getElementById('canvas');
+        this.ctx = this.canvas.getContext('2d');
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
     }
     
     draw() {
-        this.ctx = this.canvas.getContext('2d');
-        model.ant.draw(this.ctx);
+        this.ctx.fillStyle = 'darkgreen';
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        model.ant.draw(200, 200, 1.0, 'Maroon');
     }
 }
